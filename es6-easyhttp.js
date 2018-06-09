8809afbbd41d47ce4198c74d0bcfb2e852769f88
Joe Park-Kennaby <jparkkennaby@gmail.com>
2018-06-09 20:04:04 +0100

    update easyHTTP methods to use async function declaration and await operator

--- es6-easyhttp.js
+++ es6-easyhttp.js
@@ -3,81 +3,138 @@
 * Library for making HTTP requests
 
 
-* @version 2.0.0
+* @version 3.0.0
 * @author Joe Park-Kennaby
 * @license MIT
 *
 **/
 
+// MDN webdocs
+// The async function declaration defines an asynchronous function, which returns an AsyncFunction object.
+// The await operator is used to wait for a Promise. It can only be used inside an async function.
+
+
 class EasyHTTP {
 
-  get(url) {
-    // Make a HTTP GET Request (Not asynchronous)
-    //   fetch(url)
-    //   .then(res => res.json())
-    //   .then(data => data)
-    //   .catch(err => err)
-    // }
+  // get(url) {
+  //   // Make a HTTP GET Request (Not asynchronous)
+  //   //   fetch(url)
+  //   //   .then(res => res.json())
+  //   //   .then(data => data)
+  //   //   .catch(err => err)
+  //   // }
+  //
+  //   // Make a HTTP GET Request using a promise (asynchronous)
+  //   return new Promise((resolve, reject) => {
+  //     fetch(url)
+  //     .then(response => response.json())
+  //     .then(data => resolve(data))
+  //     .catch(err => reject(err))
+  //   });
+  // }
 
-    // Make a HTTP GET Request using a promise (asynchronous)
-    return new Promise((resolve, reject) => {
-      fetch(url)
-      .then(response => response.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err))
-    });
+  async get(url) {
+    const response = await fetch(url);
+
+    const responseData = await response.json();
+    return responseData;
   }
 
-  post(url, data) {
+  // post(url, data) {
+  //
+  //   // Make a HTTP GET Request using a promise (asynchronous)
+  //   return new Promise((resolve, reject) => {
+  //     fetch(url, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-type': 'application/json'
+  //       },
+  //       body: JSON.stringify(data)
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => resolve(data))
+  //     .catch(err => reject(err))
+  //   });
+  // }
+
+  async post(url, data) {
 
     // Make a HTTP GET Request using a promise (asynchronous)
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-      .then(response => response.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err))
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
     });
+
+    const responseData = await response.json();
+    return responseData;
   }
 
 
-  put(url, data) {
+
+  // put(url, data) {
+  //
+  //   // Make a HTTP PUT Request using a promise (asynchronous)
+  //   return new Promise((resolve, reject) => {
+  //     fetch(url, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-type': 'application/json'
+  //       },
+  //       body: JSON.stringify(data)
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => resolve(data))
+  //     .catch(err => reject(err))
+  //   });
+  // }
+
+  async put(url, data) {
 
     // Make a HTTP PUT Request using a promise (asynchronous)
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: 'PUT',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-      .then(response => response.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err))
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
     });
+
+    const responseData = await response.json();
+    return responseData;
+
   }
 
-  delete(url) {
+  // delete(url) {
+  //
+  //   // Make a HTTP DELETE Request using a promise (asynchronous)
+  //   return new Promise((resolve, reject) => {
+  //     fetch(url, {
+  //       method: 'DELETE',
+  //       headers: {
+  //         'Content-type': 'application/json'
+  //       }
+  //     })
+  //     .then(response => response.json())
+  //     .then(() => resolve('Resource Deleted'))
+  //     .catch(err => reject(err))
+  //   });
+  // }
+
+  async delete(url) {
 
     // Make a HTTP DELETE Request using a promise (asynchronous)
-    return new Promise((resolve, reject) => {
-      fetch(url, {
+
+      const resonse = await fetch(url, {
         method: 'DELETE',
         headers: {
           'Content-type': 'application/json'
         }
       })
-      .then(response => response.json())
-      .then(() => resolve('Resource Deleted'))
-      .catch(err => reject(err))
-    });
+      const responseData = await 'Recourse Deleted';
+      return responseData;
   }
-
-
 }
